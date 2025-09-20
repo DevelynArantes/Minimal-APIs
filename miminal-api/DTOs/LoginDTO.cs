621d8e414c1b3a18b10ef260a030f559667a621a
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-09-19 22:46:30 -0300

    Update LoginDTO.cs

--- miminal-api/DTOs/LoginDTO.cs
+++ miminal-api/DTOs/LoginDTO.cs
@@ -7,6 +7,7 @@ namespace miminal_api.DTOs
 {
     public class LoginDTO
     {
-        
+         public string Username { get; set; } = string.Empty;
+         public string Password { get; set; } = string.Empty;
     }
 }
