70ec94f4b55e52a6e9ff75dd270865f8d2847b97
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-09-19 22:53:12 -0300

    Update TokenService.cs

--- miminal-api/Services/TokenService.cs
+++ miminal-api/Services/TokenService.cs
@@ -7,6 +7,28 @@ namespace miminal_api.Services
 {
     public class TokenService
     {
-        
+          private static string secretKey = "SUA_CHAVE_SECRETA_AQUI";
+
+        // Método para gerar token JWT
+        public static string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] 
+                { 
+                    new Claim(ClaimTypes.Name, username) 
+                }),
+                Expires = DateTime.UtcNow.AddHours(1), // Expiração em 1 hora
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), 
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
