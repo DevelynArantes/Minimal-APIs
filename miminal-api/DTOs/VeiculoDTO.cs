588aebe1849aea7b7417153cba47065f83b2d067
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-09-19 22:47:59 -0300

    Update VeiculoDTO.cs

--- miminal-api/DTOs/VeiculoDTO.cs
+++ miminal-api/DTOs/VeiculoDTO.cs
@@ -7,6 +7,9 @@ namespace miminal_api.DTOs
 {
     public class VeiculoDTO
     {
-        
+        public string Make { get; set; } = string.Empty;       
+        public string Model { get; set; } = string.Empty;      
+        public int Year { get; set; }                          
+        public string LicensePlate { get; set; } = string.Empty;
     }
 }
