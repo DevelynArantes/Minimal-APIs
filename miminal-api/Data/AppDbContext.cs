d9e1fc9b953e5b6f76e1e84b5709387e467098cf
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-09-19 22:50:09 -0300

    Update AppDbContext.cs

--- miminal-api/Data/AppDbContext.cs
+++ miminal-api/Data/AppDbContext.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace miminal_api.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+         public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet para os veículos
+        public DbSet<Vehicle> Vehicles { get; set; } = null!;
     }
 }
